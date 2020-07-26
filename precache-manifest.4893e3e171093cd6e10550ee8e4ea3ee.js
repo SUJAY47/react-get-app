26b24b5ae432fa2efa43966f7523fb50a74c9fae
SUJAY47 <sujay.pmna@gmail.com>
2020-07-27 00:38:49 +0530

    Updates

---
+++ precache-manifest.4893e3e171093cd6e10550ee8e4ea3ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43aeb039b8182e9dbdf1baf31e98b9ce",
+    "url": "/react-get-app/index.html"
+  },
+  {
+    "revision": "bb3ad886fa26d2120b91",
+    "url": "/react-get-app/static/css/main.a9fb8228.chunk.css"
+  },
+  {
+    "revision": "4afc1c5da468b603254b",
+    "url": "/react-get-app/static/js/2.4c814181.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-get-app/static/js/2.4c814181.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb3ad886fa26d2120b91",
+    "url": "/react-get-app/static/js/main.fead8044.chunk.js"
+  },
+  {
+    "revision": "36785f887dce65649f6b",
+    "url": "/react-get-app/static/js/runtime-main.fc651fce.js"
+  }
+]);
